384d4dbd8741c88b808ec50aeb759bbfdd9da8a2
Andreas Langenhagen <andreas@langenhagen.cc>
2020-04-23 21:47:25 +0200

    Refactor bookmark.hpp member names and docs

--- include/bookmark.hpp
+++ include/bookmark.hpp
@@ -14,22 +14,22 @@ namespace barn {
 namespace bbm {
 
 /*Stores textual content fetched from a web page.*/
-struct PageContent {
+struct WebpageContent {
     std::string title;      /*Title of the web page.*/
     std::string content;    /*Condensed textual content of a web page.*/
-    DateTime date;          /*Timestamp from when the data has been fetched.*/
+    DateTime fetch_time;    /*Timestamp when the data has been downloaded.*/
 };
 
 /*Representation of a bookmark.*/
 struct Bookmark {
-    std::string url;                        /*Target URL.*/
-    DateTime created;                       /*Creation timestamp.*/
-    DateTime last_access;                   /*Optional last access time; all fields 0 if invalid.*/
-    std::string name;                       /*Optional name of the bookmark.*/
-    int rating;                             /*Optional 1-5 star rating; 0 means not rated.*/
-    std::vector<std::string> tags;          /*User-provided optional tags.*/
-    std::string comment;                    /*User-provided optional comment.*/
-    std::shared_ptr<PageContent> content;   /*Optional webpage text content, null if missing.*/
+    std::string url;                            /*Target URL.*/
+    DateTime created;                           /*Creation timestamp.*/
+    DateTime last_access;                       /*Optional last access time; fields 0 if invalid.*/
+    std::string name;                           /*Optional name of the bookmark.*/
+    int rating;                                 /*Optional 1-5 star rating; 0 means not rated.*/
+    std::vector<std::string> tags;              /*User-provided optional tags.*/
+    std::string comment;                        /*User-provided optional comment.*/
+    std::shared_ptr<WebpageContent> content;    /*Optional webpage text content; null if missing.*/
 };
 
 } // namespace bbm
