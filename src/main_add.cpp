c0d7634d0fab1541ae93456fdd52948332e365c4
Andreas Langenhagen <andreas@langenhagen.cc>
2020-05-01 14:54:16 +0200

    Omit printing settings in main_add.cpp

--- src/main_add.cpp
+++ src/main_add.cpp
@@ -20,15 +20,6 @@ int main(int argc, const char* argv[]) {
     const auto options = parse_options(argc,argv);
     const auto settings = load_settings(options.settings_path);
 
-    std::string url;
-    std::string title;
-    if( !fetch_url_and_title(url, title)) {
-        log(ERROR) << "Could not fetch url and title." << std::endl;
-        exit(exitcode::WRONG_INPUT);
-    }
-    Bookmark bookmark{std::move(url), DateTime::now(), DateTime::now(), std::move(title)};
-    save_bookmark(std::move(bookmark), settings.bookmarks_root_path / "subpath");
-
     x11::App app;
     app.run();
 
